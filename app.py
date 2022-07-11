054c65ecb3ae81e6711056e2bcf4fe4bf7414dcf
artem <a.tishchenko.la@gmail.com>
2022-07-11 19:45:51 +0300

    Solution added

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from celery import Celery
+
+app = Flask(__name__)
+app.config["CELERY_BROKER_URL"] = "redis://localhost:6379"
+
+celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
+celery.conf.update(app.config)
+
+@celery.task()
+def add(x,y):
+    return x+y
+
+@app.route('/')
+def add_task():
+    for i in range(10000):
+        add.delay(i, i)
+    return jsonify({'status': 'ok'})
